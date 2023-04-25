94d935aa9accbba75b3add67632370be4ca8bf31
Barbara Odozi <barbs.nene@gmail.com>
2023-04-25 14:03:04 +0100

    feat: add nodemailer logic

--- api/cron/cron-remind.js
+++ api/cron/cron-remind.js
@@ -1,7 +1,49 @@
 const cron = require("node-cron");
+const nodemailer = require("nodemailer");
+const User = require("../models/User");
 
-const job = cron.schedule("* * * * *", () => {
-  console.log("Running cron job");
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
+
+//schedule reminder
+const job = cron.schedule("*/10 * * * * *", async () => {
+  console.log("Running reminder job...");
+  const today = new Date();
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  const users = await User.find({
+    date: {
+      ">=": today,
+      "<": tomorrow,
+    },
+  }).catch((error) => {
+    console.log(`Error querying users: ${error}`);
+  });
+  users.forEach((user) => {
+    const mailOptions = {
+      from: "barbarannegarnet@gmail.com",
+      to: user.email,
+      subject: "Birthday Reminder",
+      text: `Hi ${user.name}, Just a friendly reminder that ${user.name}'s birthday is coming up tomorrow!`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent successfully!");
+        console.log(info.response);
+      }
+    });
+  });
 });
 
 module.exports = job;
---
+++ api/helpers/birthdayHelper.js
@@ -0,0 +1,24 @@
+const User = require("../models/User");
+
+module.exports = {
+  friendlyName: "Retrieve birthdays for the next day",
+
+  description: "Retrieves all users whose birthday is tomorrow",
+
+  inputs: {},
+
+  exits: {},
+
+  async fn(inputs) {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const users = await User.find({
+      birthdate: {
+        ">=": today,
+        "<": tomorrow,
+      },
+    });
+    return users;
+  },
+};
